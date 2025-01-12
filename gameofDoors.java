b859374a614db84acd4b0db8b7a8a71a7d6fc546
Swapneel14 <sarkarswapneel14@gmail.com>
2025-01-12 23:04:17 +0530

    Add files via upload

---
+++ gameofDoors.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class gameofDoors {
+    public static void main(String []args){
+        Scanner sc = new Scanner (System.in);
+        int tst= sc.nextInt();
+        
+        for(int i=1;i<=tst;i++){
+            int l=sc.nextInt();
+        int r=sc.nextInt();
+        int L = sc.nextInt();
+        int R= sc.nextInt();
+            if(r<L||R<l){
+                System.err.println(1);
+            }
+            else if(L==r||l==R){
+                System.out.println(2);
+            }
+            else if(l<L){
+                if(r<R){
+                    System.out.println((r-L)+2);
+                }
+                else if(r==R){
+                    System.out.println((r-L)+1);
+                }
+                else{
+                    System.out.println(R-L+2);
+                }
+            }
+                else if(L<l){
+                    if(R<r){
+                        System.out.println(R-l+2);
+                    }
+                    else if(R==r){
+                        System.out.println(R-l+1);
+                    }
+                    else{
+                        System.out.println(r-l+2);
+                    }
+                    
+                }
+                else{
+                    System.out.println(0);
+                }
+                System.out.println();
+            }
+            
+        }
+    }
+

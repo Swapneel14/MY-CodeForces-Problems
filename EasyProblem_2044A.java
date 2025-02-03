fb2ed36b33a460f0e5f7ddb6cf7dcc27ad3ba8ed
Swapneel14 <sarkarswapneel14@gmail.com>
2025-02-03 23:41:31 +0530

    Add files via upload

---
+++ EasyProblem_2044A.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class EasyProblem_2044A {
+
+    public static void main(String []args){
+          Scanner sc= new Scanner(System.in);
+          int tstcase= sc.nextInt();
+          for(int i=1;i<=tstcase;i++){
+            int n= sc.nextInt();
+            System.out.println(n-1);
+          }
+    }
+}

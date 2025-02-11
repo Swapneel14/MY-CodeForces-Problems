752bc3b409cf26485e72f8d61e30800fff283ae3
Swapneel14 <sarkarswapneel14@gmail.com>
2025-02-11 12:15:48 +0530

    Add files via upload

---
+++ domio_Piling.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class domio_Piling {
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+         int d,m,n;
+         m= sc.nextInt();
+         n= sc.nextInt();
+         d=(m*n)/2;
+         System.out.println(d);
+    }
+}
